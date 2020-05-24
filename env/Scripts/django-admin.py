6a0ba2c0c80880855561a13937f07aff81ed7b1c
Bjartur Thorhallsson <bjartur20@gmail.com>
2020-05-24 20:59:07 +0000

    Started working on Django

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\KillerCookie\Desktop\Python fikterí\learning_django\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
